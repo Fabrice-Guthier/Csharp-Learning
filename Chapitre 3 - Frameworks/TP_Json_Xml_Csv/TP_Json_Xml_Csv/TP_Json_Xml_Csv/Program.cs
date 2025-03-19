aae81d92457dcf1f24486245fa95bb4b06d87cf1
Guthier Fabrice <fabrice.guthier@gmail.com>
2025-03-19 15:51:03 +0100

    19-03-25 tp

---
+++ Chapitre 3 - Frameworks/TP_Json_Xml_Csv/TP_Json_Xml_Csv/TP_Json_Xml_Csv/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Json_Xml_Csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  Game of Tri:
+            //  Calculer la moyenne des scores et des successsRates(puis, dans un second temps, en ignorant les scores de 0)
+            //  Faire une classe static LanguageManager qui converti une clé de Mat en son nom dans le langage actuel
+
+
+            //  Et si vous voulez vous pouvez commencer à essayer de trifouiller le fichier XML que je vous ai mis sur le Drive et / ou les fichiers JSON du site Json Example
+
+        }
+    }
+}
